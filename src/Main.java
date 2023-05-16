43187356456c92f09cc3f95a969ef8c3eb5d8646
Alx <pokrovskiyalex77@gmail.com>
2023-05-16 16:18:40 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,25 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        int balance = 500;
+        int deposit = 5000;
+
+        if (deposit >= 1000) {
+
+            int bonus = deposit / 100;
+            int x = balance + deposit + bonus;
+
+            System.out.println("Итоговый счёт:" + x);
+            System.out.println("Бонус за пополнение:" + bonus);
+
+        } else {
+
+            int y = balance + deposit;
+
+            System.out.println("Итоговый счёт:" + y);
+            System.out.println("Бонус за пополнение:0");
+
+        }
+    }
+}
